525b740bf961ace557ead0d1137ca1de460b1248
Quanalack <maltehesk@hotmail.com>
2017-10-02 12:21:53 +0200

    Comments on commandwords

--- CommandWord.java
+++ CommandWord.java
@@ -6,15 +6,18 @@ package worldofzuul;
  */
 public enum CommandWord
 {
+    //Predefined enums
     GO("go"), QUIT("quit"), HELP("help"), UNKNOWN("?");
     
     private String commandString;
     
+    //Setter method
     CommandWord(String commandString)
     {
         this.commandString = commandString;
     }
     
+    //getter method
     public String toString()
     {
         return commandString;
