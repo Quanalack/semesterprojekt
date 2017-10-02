cb69a9617680036654879c2ed311bddd9609918e
Quanalack <maltehesk@hotmail.com>
2017-10-02 12:19:32 +0200

    Commented on commands

--- Command.java
+++ Command.java
@@ -10,27 +10,33 @@ public class Command
     private CommandWord commandWord;
     private String secondWord;
 
+    //Constructor for command class
+    //Creates a command with two words
     public Command(CommandWord commandWord, String secondWord)
     {
         this.commandWord = commandWord;
         this.secondWord = secondWord;
     }
 
+    //Getter method to get the commandword
     public CommandWord getCommandWord()
     {
         return commandWord;
     }
 
+    //Getter metod to get the second word 
     public String getSecondWord()
     {
         return secondWord;
     }
 
+    //Method to return true or false whether the typed commandword exists or not
     public boolean isUnknown()
     {
         return (commandWord == CommandWord.UNKNOWN);
     }
 
+    //Method to determine wheter a command has a seconds word. True or false
     public boolean hasSecondWord()
     {
         return (secondWord != null);
