5c5a7457ae13b78eb04b81261152269dfc24297f
BjarkeHedegaard <32387888+BjarkeHedegaard@users.noreply.github.com>
2017-10-13 10:57:34 +0200

    commentary

--- MotelMurders/src/motelmurders/Command.java
+++ MotelMurders/src/motelmurders/Command.java
@@ -5,32 +5,38 @@ package motelmurders;
  * @version 2006.03.30
  */
 
+//Class Command
 public class Command
 {
     private CommandWord commandWord;
     private String secondWord;
 
+    //Constructor, Command
     public Command(CommandWord commandWord, String secondWord)
     {
         this.commandWord = commandWord;
         this.secondWord = secondWord;
     }
 
+    //Getter
     public CommandWord getCommandWord()
     {
         return commandWord;
     }
 
+    //Getter
     public String getSecondWord()
     {
         return secondWord;
     }
 
+    //Return unknown
     public boolean isUnknown()
     {
         return (commandWord == CommandWord.UNKNOWN);
     }
 
+    //Return secondword
     public boolean hasSecondWord()
     {
         return (secondWord != null);
