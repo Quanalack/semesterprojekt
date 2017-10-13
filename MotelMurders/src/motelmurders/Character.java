e1d2ff72eb1a43372cd5c76e25d487a448275ec4
BjarkeHedegaard <32387888+BjarkeHedegaard@users.noreply.github.com>
2017-10-13 10:53:37 +0200

    Comments

--- MotelMurders/src/motelmurders/Character.java
+++ MotelMurders/src/motelmurders/Character.java
@@ -9,47 +9,58 @@ package motelmurders;
  *
  * @author malte
  */
+
+//Class Create characters
 public class Character {
     private String name;
     private String description;
     private boolean isMurderer;
     private Room currentRoom;
-    
+        
+        //Constructer Character: Name, Description, Room
         public Character(String name, String description, Room startRoom) {
                 this.name = name;
                 this.description = description;
-                this.isMurderer = false;
+                this.isMurderer = false;    //False by default?????
                 this.currentRoom = new Room(startRoom.getLongDescription(), startRoom.getRoomName());
         }
 
+    //Getter
     public String getName() {
         return name;
     }
-
+    
+    //Setter
     public void setName(String name) {
         this.name = name;
     }
-
+    
+    //Getter
     public String getDescription() {
         return description;
     }
 
+    //Setter
     public void setDescription(String description) {
         this.description = description;
     }
         
+        //Movement from one to another, change current room
         public void goRoom(Room newRoom) {
             this.currentRoom = newRoom;
         }
         
+        //Getter
         public Room getCurrentRoom() {
             return this.currentRoom;
         }
         
+        //Setter
         public void setIsMurderer(boolean bool) {
             isMurderer = bool;
         }
         
+        //Getter
         public boolean getIsMurderer () {
             return isMurderer;
         }
