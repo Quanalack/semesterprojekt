72e7fbc6b977074e057043ebf2724c32390d8c16
hrmalte <maltehesk@hotmail.com>
2017-10-23 12:25:06 +0100

    Limited inventory
    Added a limit of 4 items to inventory including the magnifying glass

--- MotelMurders/src/motelmurders/Game.java
+++ MotelMurders/src/motelmurders/Game.java
@@ -10,6 +10,7 @@ public class Game
 {
     private Parser parser;
     private Room currentRoom;
+    private int maxLimit = 4; //Number of items in inventory including magnifying glass
 
     public Room getCurrentRoom() {
         return currentRoom;
@@ -247,11 +248,10 @@ public class Game
             goRoom(command);
             getCharacterString();
             
+            //15 % chance of moving cleaning Lady
             if (Math.random() <= 0.15) {
                 moveCleaningLady();
             }
-            
-
         }
         else if (commandWord == CommandWord.QUIT) {
             //Method to quit game
@@ -279,6 +279,11 @@ public class Game
     }
     private void pickupItem(Command command) 
     {
+        
+        if (inventory.size() == maxLimit) {
+            System.out.println("No more room");
+        }
+        else {
         if(!command.hasSecondWord()) {
             System.out.println("Pick up what?");
             return;
@@ -298,6 +303,7 @@ public class Game
             
             System.out.println("Picked up:" + item);
             
+        }
         }
     }
     
