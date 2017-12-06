21e8bc24907b180632b59fd9a29374289f729cdd
hrmalte <maltehesk@hotmail.com>
2017-12-06 13:30:17 +0100

    SaveFile class

---
+++ MotelMurders/src/Data/SaveFile.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Data;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author malte
+ */
+public class SaveFile {
+    
+    //The arraylist will hold the characters/NPC's for our game
+    ArrayList<Object> NPC = new ArrayList<>();
+
+    //Holds the rooms for the game
+    ArrayList<Object> rooms = new ArrayList<>();
+
+    //Holds the main character/player of the game
+    Object player;
+    
+    public SaveFile(ArrayList<Object> NPC, ArrayList<Object> rooms, Object player){
+        this.NPC = NPC;
+        this.player = player;
+        this.rooms = rooms;
+        
+    }
+
+    public ArrayList<Object> getNPC() {
+        return NPC;
+    }
+
+    public ArrayList<Object> getRooms() {
+        return rooms;
+    }
+
+    public Object getPlayer() {
+        return player;
+    }
+
+    
+}
