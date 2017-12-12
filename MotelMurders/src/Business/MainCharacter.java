ac471cc8fc4da0b4a50deab1d962514ae33dee42
hrmalte <maltehesk@hotmail.com>
2017-12-12 13:53:15 +0100

    Javadoc and comments on Maincharacter

--- MotelMurders/src/Business/MainCharacter.java
+++ MotelMurders/src/Business/MainCharacter.java
@@ -8,34 +8,36 @@ package Business;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-/**
- *
- * @author malte
- */
 public class MainCharacter implements IPerson {
 
     private String name; //Name of the player
     private Room currentRoom; //Room of the player
-    private String description;
+    private String description; //Players description
 
-    public String getName() {
-        return name;
-    }
-
-    //Method to print players current inventory
     private final int maxLimit = 4; //Number of items in inventory including magnifying glass. Can only be assigned once
 
     //Our arraylist will hold the inventory items for our game
     ArrayList<Clue> inventory = new ArrayList<>();
 
+    /**
+     * Get the inventory of player
+     * @return An arraylist of the inventory
+     */
     public ArrayList<Clue> getInventory() {
         return inventory;
     }
 
+    /**
+     * Set the players inventory
+     * @param inventory the new inventory to be set
+     */
     public void setInventory(ArrayList<Clue> inventory) {
         this.inventory = inventory;
     }
 
+    /**
+     * Print current inventory to console
+     */
     public void printInventory() {
         String output = ""; //Empty string to output
         System.out.println(">You're currently carrying:");
@@ -48,24 +50,36 @@ public class MainCharacter implements IPerson {
         System.out.println(output);
     }
 
+    /**
+     * Adds the magnifying glass to players inventory
+     */
     public void addMagnifyingGlass() {
 
         inventory.add(new Clue("","Magnifying Glass",true));
     }
 
+    /**
+     * Drops a specific item from inventory
+     * @param command The command input from player
+     */
     public void dropItem(Command command) {
+        
+        //Check if there is more than one word
         if (!command.hasSecondWord()) {
             System.out.println("Drop what?");
             return;
         }
 
+        //get the item name
         String item = command.getSecondWord();
 
-        if (item.equalsIgnoreCase("magnifying glass")) {
+        //Check if player is trying to drop magnifying glass
+        if (item.equalsIgnoreCase("magnifying")) {
             System.out.println(">You cannot drop your magnifying glass! You need it!");
             return;
         }
         
+        //What is this???? Please check this on 13-12-2017. From line 82-107
         Clue nextItem = null;
         int index = 0;
         for (int i = 0; i < inventory.size(); i++) {
@@ -93,19 +107,30 @@ public class MainCharacter implements IPerson {
         }
     }
 
+    /**
+     * Pick up a specific item from current room
+     * @param command the command from user input
+     */
     public void pickupItem(Command command) {
 
+        //Check if player has space in inventory
         if (inventory.size() == maxLimit) {
             System.out.println(">There is no more room in your inventory.");
         } else {
+            
+            //Check if user command has a second word
             if (!command.hasSecondWord()) {
                 System.out.println(">Pick up what?");
                 return;
             }
+            
+            //Get second word from user command
             String item = command.getSecondWord();
 
+            //Get item by the name from command
             Item nextItem = getCurrentRoom().getItem(item);
 
+            //Check if item exist
             if (nextItem == null) {
                 System.out.println(">There is no item named that!");
                 
@@ -113,6 +138,7 @@ public class MainCharacter implements IPerson {
                 System.out.println(">You can't pick up stuff like that.");
                 
             } else {
+                //add item to inventory and remove from room.
                 inventory.add((Clue)nextItem);
                 getCurrentRoom().removeItem(item);
                 
@@ -122,6 +148,18 @@ public class MainCharacter implements IPerson {
         }
     }
 
+    /**
+     * Get the name of the player
+     * @return the name of the player.
+     */
+    @Override
+    public String getName() {
+        return name;
+    }
+    
+    /**
+     * Sets the name of the player from user input.
+     */
     public void newName() {
         Scanner output = new Scanner(System.in);
 
@@ -133,21 +171,37 @@ public class MainCharacter implements IPerson {
 
     }
 
+    /**
+     * Change current room.
+     * @param newRoom the room to go to.
+     */
     @Override
     public void goRoom(Room newRoom) {
         currentRoom = newRoom;
     }
 
+    /**
+     * Get the current room
+     * @return the current room the player is in.
+     */
     @Override
     public Room getCurrentRoom() {
         return this.currentRoom;
     }
 
+    /**
+     * Get the description of the player
+     * @return the description of the player
+     */
     @Override
     public String getDescription() {
         return this.description;
     }
 
+    /**
+     * Set player description
+     * @param description Description of player
+     */
     @Override
     public void setDescription(String description) {
         this.description = description;
