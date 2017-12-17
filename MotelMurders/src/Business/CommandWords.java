e0a56d51bd2fce61855b5fd1519944c7ce7883b8
Quanalack <maltehesk@hotmail.com>
2017-12-17 15:51:25 +0100

    javadoc for CommandWords

--- MotelMurders/src/Business/CommandWords.java
+++ MotelMurders/src/Business/CommandWords.java
@@ -4,11 +4,18 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 
-
+/**
+ * Contains all the commands 
+ * 
+ */
 public class CommandWords
 {
+    //Hashmap with all the commands
     private HashMap<String, CommandWord> validCommands;
 
+    /**
+     * Constructor puts all valid commands into validCommands hashmap
+     */
     public CommandWords()
     {
         validCommands = new HashMap<String, CommandWord>();
@@ -19,6 +26,11 @@ public class CommandWords
         }
     }
 
+    /**
+     * Get a commandword from a string (player input)
+     * @param commandWord the player input
+     * @return the command from the list or unknown
+     */
     public CommandWord getCommandWord(String commandWord)
     {
         CommandWord command = validCommands.get(commandWord);
@@ -30,11 +42,20 @@ public class CommandWords
         }
     }
     
+    /**
+     * Check if a string (player input) is a valid command
+     * @param aString player input string
+     * @return true if command exists
+     */
     public boolean isCommand(String aString)
     {
         return validCommands.containsKey(aString);
     }
 
+    /**
+     * Shows all the calid commands
+     * @return an arrayList of the valid commands
+     */
     public ArrayList<String> showAll() 
     {
         ArrayList<String> commands = new ArrayList<>();
